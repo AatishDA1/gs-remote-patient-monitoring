b34f6397d11e86b2b3b8a461760bc6bfb24bcaa0
ATYSzeto <ays20@ic.ac.uk>
2022-12-30 17:34:04 +0000

    cleanedup

--- src/pages/ManageDoctors.js
+++ src/pages/ManageDoctors.js
@@ -5,7 +5,6 @@ import GenHeader from "../components/General/GenHeader";
 import GenFooter from "../components/General/GenFooter";
 import MDHeading from"../components/ManagingDrs/MDHeading"
 import MDActions from "../components/ManagingDrs/MDActions";
-import ChangeBtnState from "../components/ManagingDrs/ChangeBtnState";
 
 
 const ManageDoctors = () => (

85198aa28114018be719996a14ebb82afe52d13a
ls3020 <ls3020@ic.ac.uk>
2023-01-04 05:31:30 +0000

    this is ready to plot graphs ! i put a comment around where they should be plotted its going to be messy but i could not figure out any other way to do it im very sorry about that

--- src/pages/PatientSelect.js
+++ src/pages/PatientSelect.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import db from "./firebase";
 
-import Popup from '../components/General/popupPTV'
-
 import PatientAddButton from  "../components/HomePages/PatientAddButton";
 
 function PatientSelect(){
-	const [buttonPopup, setButtonPopup] = useState(false);
+
     const [patientsData, setPatientsData] = useState([]);
 
     useEffect(() => {
@@ -22,12 +20,72 @@ function PatientSelect(){
         }, 
     []);
 
-return(<>
 
-	  <div className="App__DataDisplay">
-	  <div className="App__buttons">
+	const [readData, setreadData] = useState("");
+	const [getName, setGetName] = useState("");
+	const [getResp, setGetResp] = useState("");
+	const [getHR, setGetHR] = useState("");
+	const [getSys, setGetSys] = useState("");
+	const [getDia, setGetDia] = useState("");
+	const [getTemp, setGetTemp] = useState("");
+
+	const getData = (e) => {
+		e.preventDefault();
+		const please = db.collection("patientsData").doc(readData)
+
+		const doc = please.get({
+			name : getName,
+			resp: getResp,
+			temp: getTemp,
+			sys: getSys,
+			dia: getDia,
+			hr: getHR
+		});
+
+
+
+		if (!doc.exists) {
+			<>'No such document!'</>;
+		  } else {
+
+			setGetName(doc.name);
+			setGetResp(doc.resp);
+			setGetTemp(doc.temp);
+			setGetSys(doc.sys);
+			setGetDia(doc.dia);
+			setGetHR(doc.hr);
+			setreadData("");
+		  }
+		  
+	
+
+	};
+
+
+return(<>
+	
+	<div className="App">
+	{!readData ? (
+		
+		<div className="App__buttons">
 		<PatientAddButton/>
 		</div>
+		) : (
+			/*PLOT GRAPHS UNDER HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+		<div className="App__Updateform">
+				<h2>Patient Vitals</h2>
+				<button onClick={getData}>fetch</button> 
+				Name : {getName}
+				Resp : {getResp}
+				Temp : {getTemp}
+				Sys : {getSys}
+				Dia : {getDia}
+				HR : {getHR}
+				</div>
+		)}
+
+
+	  <div className="App__DataDisplay">
 		<table>
 			<tr>
 			<th>NAME</th>
@@ -45,26 +103,22 @@ return(<>
 			<td>{data.gender}</td>
 			<td>{data.bedNO}</td>
 			<td>
-			<button onClick={() => 
-				 {setButtonPopup(true)}
-				 }> View Details </button>
-				<Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}>
-					<h2>Patient Vitals</h2>
-
-					<td>{data.resp}</td>
-					<td>{data.temp}</td>
-					<td>{data.sys}</td>
-					<td>{data.dia}</td>
-					<td>{data.hr}</td>
-					
-					
-				</Popup>
-			
+				<>
+				<button onClick={() => {setreadData(id);
+					setGetName(data.name);
+					setGetResp(data.resp);
+					setGetTemp(data.temp);
+					setGetSys(data.sys);
+					setGetDia(data.dia);
+					setGetHR(data.hr);
+     				}}> View Details </button>
+				</>
 			</td>
 			</tr>
 		))}
 		
 		</table>
+		</div>
 	</div>
 	</>
 );

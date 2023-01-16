c208fe87330122d869d5079a768e2f3a0c6fb8e1
ATYSzeto <ays20@ic.ac.uk>
2023-01-16 01:41:19 +0000

    semi colon deleted

--- src/components/Simulation/Simulation.js
+++ src/components/Simulation/Simulation.js
@@ -98,7 +98,8 @@ export default function Simulation() {
     setWarning(true);
     setCritical(false);
     setTimeout(() => {
-      setButtonPopup(true); play()
+      setButtonPopup(true)
+      play()
       setsubmitwarnPopup(true);
     }, 10000);
   };
@@ -108,7 +109,8 @@ export default function Simulation() {
     setCritical(true);
 
     setTimeout(() => {
-      setButtonCritPopup(true); play()
+      setButtonCritPopup(true)
+      play()
       setsubmitBtnPopup(true);
     }, 10000);
   };

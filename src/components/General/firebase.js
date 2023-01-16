e91a3003169c3c61c5ef28b392cbcd2295617f91
denisewong001 <cww20@ic.ac.uk>
2023-01-16 06:55:23 +0000

    testbuild for firebase

--- src/components/General/firebase.js
+++ src/components/General/firebase.js
@@ -12,13 +12,13 @@ import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATdnKOGZaaTViW3EdN3gwkQyqANGs4xb0",
-  authDomain: "gilbert2-329ad.firebaseapp.com",
-  projectId: "gilbert2-329ad",
-  storageBucket: "gilbert2-329ad.appspot.com",
-  messagingSenderId: "14716882201",
-  appId: "1:14716882201:web:a4131d6b01038f6fab4988",
-  measurementId: "G-1075B2QKJF"
+  apiKey: "AIzaSyDT2jq_v4nLa3hGmzhr1Z9LdzAKFC1sHRA",
+  authDomain: "gilbertsolutions-88dc8.firebaseapp.com",
+  projectId: "gilbertsolutions-88dc8",
+  storageBucket: "gilbertsolutions-88dc8.appspot.com",
+  messagingSenderId: "387671078978",
+  appId: "1:387671078978:web:9aeca22690b46f6b7955db",
+  measurementId: "G-QYYDPS8GBJ"
 };
 
 // Initialize Firebase
